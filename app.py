23442fefea1b704add82879981307b2d32d68dee
NegritoBo <168646692+NegritoBo@users.noreply.github.com>
2025-09-27 21:31:24 -0300

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
- from flask import Flask, request, jsonify, render_template_string
+from flask import Flask, request, jsonify, render_template_string
 from flask_cors import CORS
 import openpyxl
 import json
